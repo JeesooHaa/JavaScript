45c4a8333241da924c66a38f57f75c010e6d4123
JeesooHaa <haajeesoo@gmail.com>
2019-10-30 11:03:58 +0900

    Finish array helper method

--- day01/06_array_helper_method.js
+++ day01/06_array_helper_method.js
@@ -30,6 +30,14 @@ const doubleNumbers2 = numbers2.map(number => number ** 0.5)
 // map 뒤에 함수를 넘겨준다
 numbers2.map(Math.sqrt) // !!
 
+// map 헬퍼를 사용해서, distance / time 를 저장하는 배열 speeds 를 만들어라 
+const trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 },
+]
+
+const speeds = trips.map(speed => speed.distance / speed.time)
 
 // filter - 주어진 함수의 테스트를 통과하는 모든 요소를 모아서 새로운 배열 반환 
 
@@ -46,3 +54,70 @@ const products = [
 
 const fruits = products.filter(product => product.type === 'fruit')
 console.log(fruits)
+
+// filter - 나이가 50 이상인 아임템만 가지고 있는 새로운 배열을 만드세요
+const ages = [15, 25, 35, 45, 55, 65, 75, 85, 95]
+
+const oldAges = ages.filter(age => age >= 50)
+// 두번째 인자로 index 가 들어갈 수 있다 
+// 세번째 인자로 self ! 
+// const oldAges = ages.filter((age, index, self) => age >= 50)
+
+
+// find - 주어진 판별 함수를 만족하는 첫번째 요소의 값을 반환, 없다면 undefined 반환 
+
+const users = [
+  { name: 'Tony Stark', age: 45 },
+  { name: 'Steve Rogers', age: 32 },
+  { name: 'Thor', age: 40 },
+]
+
+const tony = users.find(user => {
+  return user.name === 'Tony Stark'
+})
+// filter 는 여러개를 찾기 때문에 배열 안에 담아서 준다 / find 는 하나의 요소만! 
+
+
+// some - 배열 안의 어떤요소라도 (=== 가 true 인게 하나라도!) 주어진 판별 함수를
+// 통과하는지 테스트하고 결과에 따라 boolean 값을 반환한다 
+
+const arr = [1, 2, 3, 4, 5, ]
+// 하나의 요소라도 짝수를 포함하고 있다면 true 
+const result = arr.some(number => {
+  return number % 2 === 0 
+})
+
+
+// 모든 요소가 짝수를 포함하고 있다면 true 
+const result2 = arr.every(number => {
+  return number % 2 === 0
+})
+
+// request 배열에서 각 요청들 중 status 가 pending 인 요청이 있는지 확인하라
+const requests = [
+  { url: '/photos', status: 'complete'},
+  { url: '/albums', status: 'pending'},
+  { url: '/users', status: 'failed'},
+]
+
+const isInProgress = requests.some(request => request.status === 'pending')
+
+
+// reduce - 배열의 각 요소에 대해 주어진 'reducer' 함수를 실행하고 하나의 값을 반환한다
+// reduce 는 배열 내의 숫자 총합, 배열 내 평균 계산 등 배열의 값을 하나로 줄이는 동작을 한다 
+// 첫번째 매개변수는 `누적 값 (전 단계의 결과)`
+
+const ssafyTest = [90, 99, 78, 80]
+
+// output 뒤에 total 의 초기화값 
+const sum = ssafyTest.reduce((total, score) => {
+  total += score
+  return total // 다음 루프로 누적값을 넘김 
+}, 0)
+
+
+// ssafyTest 배열을 doubleSsafyTest 로 만드시오!
+const doubleSsafyTest = ssafyTest.reduce((doubleScores, score) => {
+  doubleScores.push(score * 2)
+  return doubleScores // 다음 루프로 누적값을 넘김 
+}, [])
